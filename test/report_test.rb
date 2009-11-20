8c0ef68809e3eb19b860c5695de851a42245b9d0
Brian Carlson <brian.m.carlson@gmail.com>
2009-11-20 13:28:56 +0800

    stringio is not required by default in ruby 1.9. Changed reference from Riot::TextReport to Riot::IOReporter

--- test/report_test.rb
+++ test/report_test.rb
@@ -1,10 +1,11 @@
 require 'teststrap'
+require 'stringio'
 
 context "TextReport" do
   context "with writer" do
     setup do
       @writer = StringIO.new
-      Riot::TextReport.new(@writer)
+      Riot::IOReporter.new(@writer)
     end
 
     asserts("prints to writer") do

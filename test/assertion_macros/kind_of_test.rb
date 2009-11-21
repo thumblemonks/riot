855096c0d027d31ff108549fab76b6f82b8c2c78
brianc <brian.m.carlson@gmail.com>
2009-11-20 18:00:37 -0600

    added_nil_test

--- test/assertion_macros/kind_of_test.rb
+++ test/assertion_macros/kind_of_test.rb
@@ -10,4 +10,8 @@ context "A kind_of assertion macro" do
   asserts ":fail when not a kind of String" do
     Riot::Assertion.new("foo") { 0 }.kind_of(String).run(topic)
   end.equals([:fail, %Q{expected kind of String, not 0}])
+  
+  asserts ":fail when nil" do
+    Riot::Assertion.new("foo") { }.kind_of(String).run(topic)
+  end.equals([:fail, %Q{expected kind of String, not nil}])
 end # A kind_of assertion macro

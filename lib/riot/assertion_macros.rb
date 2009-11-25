d87a3d1fd25f744b68d21d7cef665bd252b8add7
Brian Carlson <brian.m.carlson@gmail.com>
2009-11-24 19:31:01 -0600

    added documentation for the empty assertion

--- lib/riot/assertion_macros.rb
+++ lib/riot/assertion_macros.rb
@@ -96,7 +96,11 @@ module Riot
         pass
       end
     end
-    
+
+    # Asserts the result is empty
+    #   asserts("a string") { "" }.empty
+    #   asserts("an array") { [] }.empty
+    #   asserts("a hash") { Hash.new }.empty
     assertion(:empty) do |actual|
       actual.length == 0 ? pass : fail("expected #{actual} to be empty")
     end

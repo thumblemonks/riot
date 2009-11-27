775a6fdbdb11aef409c2fabb4574e61d0fb0f20a
Justin Knowlden <gus@gusg.us>
2009-11-27 17:10:27 -0600

    Refactoring test for includes macro to use assert test shortcuts

--- test/assertion_macros/includes_test.rb
+++ test/assertion_macros/includes_test.rb
@@ -5,14 +5,9 @@ context "An includes assertion macro" do
     Riot::Assertion.new("an array") { [1, 6, 42, 7] }
   end
 
-  asserts(":pass when expectation met") do
-    topic.includes(42).run(Riot::Situation.new) == [:pass]
+  assertion_test_passes("when array includes 42") { topic.includes(42) }
+
+  assertion_test_fails("when 99 not included in array", "expected [1, 6, 42, 7] to include 99") do
+    topic.includes(99)
   end
-
-  context "that is failing" do
-    setup { topic.includes(99).run(Riot::Situation.new) }
-
-    asserts(":fail") { topic.first == :fail }
-    asserts("message") { topic.last == %Q{expected [1, 6, 42, 7] to include 99} }
-  end # that is failing
 end # An includes assertion macro

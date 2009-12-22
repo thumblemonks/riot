09609933c8f8e09dd11ac74b4001228d009b1be0
Justin Knowlden <gus@gusg.us>
2009-12-22 08:21:42 -0600

    Polishing the turd ;)

--- lib/riot/assertion.rb
+++ lib/riot/assertion.rb
@@ -1,43 +1,34 @@
 module Riot
   class Assertion < RunnableBlock
-  
-    def initialize(description, &definition)
-      super
-      @expectings, @expect_exception, @expectation_block = [], false, nil
-      @macro = AssertionMacro.default
-    end
-
     class << self
-      def macros; @assertions ||= {}; end
+      def macros; @macros ||= {}; end
 
       def register_macro(name, assertion_macro, expect_exception=false)
         macros[name.to_s] = [expect_exception, assertion_macro.new]
       end
     end
 
-    def method_missing(name, *expectings, &expectation_block)
-      @expect_exception, @macro = self.class.macros[name.to_s]
-      if @macro
-        @expectings, @expectation_block = expectings, expectation_block
-        self
-      else
-        raise NoMethodError, name
-      end
+    def initialize(description, &definition)
+      super
+      @expectings, @expect_exception, @expectation_block = [], false, nil
+      @macro = AssertionMacro.default
     end
 
     def run(situation)
       @expectings << situation.evaluate(&@expectation_block) if @expectation_block
-      process_macro(situation, @expect_exception) do |actual|
-        @macro.evaluate(actual, *@expectings)
-      end
+      actual = situation.evaluate(&definition)
+      @macro.evaluate((@expect_exception ? nil : actual), *@expectings)
+    rescue Exception => e
+      @expect_exception ? yield(e) : @macro.error(e)
     end
 
   private
-    def process_macro(situation, expect_exception)
-      actual = situation.evaluate(&definition)
-      yield(expect_exception ? nil : actual)
-    rescue Exception => e
-      expect_exception ? yield(e) : @macro.error(e)
+    def enhance_with_macro(name, *expectings, &expectation_block)
+      @expect_exception, @macro = self.class.macros[name.to_s]
+      raise(NoMethodError, name) unless @macro
+      @expectings, @expectation_block = expectings, expectation_block
+      self
     end
+    alias :method_missing :enhance_with_macro
   end # Assertion
 end # Riot

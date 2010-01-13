535a7e6cf622d5e169bb6a5ca1e6a6c5a95c7555
Daniel Schierbeck <daniel.schierbeck@gmail.com>
2010-01-13 17:40:21 +0100

    Add a test of passing arguments to helpers

--- test/context_test.rb
+++ test/context_test.rb
@@ -116,6 +116,10 @@ end # Making a context
 
 context "A context with a helper" do
   setup { "foo" }
+
   helper(:upcase) { topic.upcase }
+  helper(:append) {|str| topic + str }
+
   asserts("executing the helper") { upcase }.equals("FOO")
+  asserts("calling a helper with an argument") { append("bar") }.equals("foobar")
 end

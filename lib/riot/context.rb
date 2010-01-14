f86cf8aecf824066c775413f42cc8371c8b250a4
Daniel Schierbeck <daniel.schierbeck@gmail.com>
2010-01-15 05:07:25 +0800

    Add documentation of Riot::Context

--- lib/riot/context.rb
+++ lib/riot/context.rb
@@ -1,8 +1,19 @@
 module Riot
   RootContext = Struct.new(:setups, :teardowns)
 
+  # You make your assertions within a Context. The context stores setup and
+  # teardown blocks, and allows for nesting and refactoring into helpers.
   class Context
-    attr_reader :description, :parent
+    # The description of the context.
+    #
+    # @return [String]
+    attr_reader :description
+
+    # The parent context.
+    #
+    # @return [Riot::Context]
+    attr_reader :parent
+
     def initialize(description, parent=nil, &definition)
       @parent = parent || RootContext.new([],[])
       @description = description
@@ -10,14 +21,40 @@ module Riot
       self.instance_eval(&definition)
     end
 
+    # Create a new test context.
+    #
+    # @param [String] description
     def context(description, &definition)
       new_context(description, self.class, &definition)
     end
     
-    def setups; @parent.setups + @setups; end
-    def teardowns; @parent.teardowns + @teardowns; end
+    # Returns an ordered list of the setup blocks for the context.
+    #
+    # @return [Array[Riot::RunnableBlock]]
+    def setups
+      @parent.setups + @setups
+    end
+
+    # Returns an ordered list of the teardown blocks for the context.
+    #
+    # @return [Array[Riot::RunnableBlock]]
+    def teardowns
+      @parent.teardowns + @teardowns
+    end
     
-    def setup(&definition) (@setups << Setup.new(&definition)).last; end
+    # Add a setup block.
+    #
+    # A setup block defines the topic of the context. There can be multiple setup
+    # blocks; each can access the previous topic through the +topic+ attribute.
+    #
+    #   context "A string" do
+    #     setup { "foo" }
+    #     setup { topic * 2 }
+    #     asserts(:length).equals(6)
+    #   end
+    def setup(&definition)
+      (@setups << Setup.new(&definition)).last
+    end
 
     # A setup shortcut that returns the original topic so you don't have to. Good for nested setups. Instead
     # of doing this in your context:
@@ -34,11 +71,44 @@ module Riot
       setup { self.instance_eval(&definition); topic }
     end
 
-    def teardown(&definition) (@teardowns << Setup.new(&definition)).last; end
+    # Add a teardown block.
+    def teardown(&definition)
+      (@teardowns << Setup.new(&definition)).last
+    end
 
-    def asserts(what, &definition) new_assertion("asserts", what, &definition); end
-    def should(what, &definition) new_assertion("should", what, &definition); end
-    def asserts_topic(what="that it"); asserts(what) { topic }; end
+    # Makes an assertion.
+    #
+    # In the most basic form, an assertion requires a descriptive name and a block.
+    #
+    #   asserts("#size is equals to 2") { topic.size == 2 }
+    #
+    # However, several shortcuts are available. Assertion macros can be added to the
+    # end, automating a number of common assertion patterns, e.g.
+    #
+    #   asserts("#size") { topic.size }.equals(2)
+    #
+    # Furthermore, the pattern of testing an attribute on the topic is codified as
+    #
+    #   asserts(:size).equals(2)
+    #
+    # Passing a Symbol to +asserts+ enables this behaviour. For more information on
+    # assertion macros, see {Riot::AssertionMacro}.
+    #
+    # @param [String, Symbol] what the property being tested
+    def asserts(what, &definition)
+      new_assertion("asserts", what, &definition)
+    end
+
+    def should(what, &definition)
+      new_assertion("should", what, &definition)
+    end
+
+    # Makes an assertion on the topic itself, e.g.
+    #
+    #   asserts_topic.matches(/^ab+/)
+    def asserts_topic(what="that it")
+      asserts(what) { topic }
+    end
 
     def run(reporter)
       reporter.describe_context(self) unless @assertions.empty?

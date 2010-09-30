08a8a255fa19a0819e6eb94f6fd51303b6625d26
Justin Knowlden <gus@gusg.us>
2010-09-30 16:19:28 -0500

    Refactored denies testing

--- test/core/context/deny_test.rb
+++ test/core/context/deny_test.rb
@@ -2,40 +2,25 @@ require 'teststrap'
 
 context "Using denies" do
 
-  context "with a test that should pass" do
-    setup do
-      Riot::Context.new("Apple Jackie") do
-        denies("with false") { false }
-      end.run(MockReporter.new)
-    end
+  asserts("result when returning false from the assertion block") do
+    report = Riot::Context.new("Apple Jackie") do
+      denies("with false") { false }
+    end.run(MockReporter.new)
+    [report.passes, report.failures, report.errors]
+  end.equals([1,0,0])
 
-    asserts("passes, failures, errors") do
-      [topic.passes, topic.failures, topic.errors]
-    end.equals([1,0,0])
-  end
+  asserts("result when returning true from the assertion block") do
+    report = Riot::Context.new("Apple Jackie") do
+      denies("with true") { true }
+    end.run(MockReporter.new)
+    [report.passes, report.failures, report.errors]
+  end.equals([0,1,0])
 
-  context "with a test that should fail" do
-    setup do
-      Riot::Context.new("Apple Jackie") do
-        denies("with true") { true }
-      end.run(MockReporter.new)
-    end
+  asserts("result when assertion block has an exception") do
+    report = Riot::Context.new("Apple Jackie") do
+      denies("ooooops") { raise Exception }
+    end.run(MockReporter.new)
+    [report.passes, report.failures, report.errors]
+  end.equals([0,0,1])
 
-    asserts("passes, failures, errors") do
-      [topic.passes, topic.failures, topic.errors]
-    end.equals([0,1,0])
-  end
-
-  context "with a test that should error" do
-    setup do
-      Riot::Context.new("Apple Jackie") do
-        denies("ooooops") { raise Exception }
-      end.run(MockReporter.new)
-    end
-
-    asserts("passes, failures, errors") do
-      [topic.passes, topic.failures, topic.errors]
-    end.equals([0,0,1])
-  end
-
-end # Calling context with deny
+end # Using denies

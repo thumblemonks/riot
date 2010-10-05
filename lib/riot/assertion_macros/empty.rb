53e64a440affad1fce0b3dbeeeec9209ae143e45
Justin Knowlden <gus@gusg.us>
2010-10-05 08:33:04 -0500

    Added negative test for the :empty assertion macro

--- lib/riot/assertion_macros/empty.rb
+++ lib/riot/assertion_macros/empty.rb
@@ -1,13 +1,24 @@
 module Riot
-  # Asserts the result is empty
+  # In the postive case, asserts the result of the test is empty.
+  #
   #   asserts("a string") { "" }.empty
   #   asserts("an array") { [] }.empty
   #   asserts("a hash") { Hash.new }.empty
+  #
+  # In the negative case, asserts the result of the test is not empty.
+  #
+  #   denies("a string") { "foo" }.empty
+  #   denies("an array") { [1] }.empty
+  #   denies("a hash") { {:foo => "bar" } }.empty
   class EmptyMacro < AssertionMacro
     register :empty
 
     def evaluate(actual)
-      actual.length == 0 ? pass : fail(expected_message(actual).to_be_empty)
+      actual.empty? ? pass(new_message.is_empty) : fail(expected_message(actual).to_be_empty)
+    end
+
+    def devaluate(actual)
+      actual.empty? ? fail(new_message.expected_to_not_be_empty) : pass(new_message.is_empty)
     end
   end
 end
--- test/core/assertion_macros/empty_test.rb
+++ test/core/assertion_macros/empty_test.rb
@@ -1,24 +1,52 @@
 require 'teststrap'
 
 context "An empty assertion macro" do
-  setup do
-    def assert_empty(string)
-      Riot::Assertion.new("test") { string }.empty
-    end
+  helper(:assert_empty) do |o|
+    Riot::Assertion.new("test") { o }.empty
   end
 
-  assertion_test_passes("when string is empty") { assert_empty("") }
+  assertion_test_passes("when string is empty", "is empty") { assert_empty("") }
   assertion_test_fails("when string has content", "expected \" \" to be empty") do
     assert_empty(" ")
   end
 
-  assertion_test_passes("when an array is empty") { assert_empty([]) }
+  assertion_test_passes("when an array is empty", "is empty") { assert_empty([]) }
   assertion_test_fails("when an array has items", "expected [1] to be empty") do
     assert_empty([1])
   end
 
-  assertion_test_passes("when a hash is empty") { assert_empty({}) }
+  assertion_test_passes("when a hash is empty", "is empty") { assert_empty({}) }
   assertion_test_fails("when a hash has items", "expected {:name=>\"washington\"} to be empty") do
     assert_empty({:name => 'washington'})
   end
 end
+
+context "A negative empty assertion macro" do
+  helper(:assert_empty) do |o|
+    Riot::Assertion.new("test", true) { o }.empty.run(Riot::Situation.new)
+  end
+
+  asserts("when string is not empty") do
+    assert_empty("foo")
+  end.equals([:pass, "is empty"])
+
+  asserts("when string is empty") do
+    assert_empty("")[0..1]
+  end.equals([:fail, "expected to not be empty"])
+
+  asserts("when array is not empty") do
+    assert_empty([1])
+  end.equals([:pass, "is empty"])
+
+  asserts("when array is empty") do
+    assert_empty([])[0..1]
+  end.equals([:fail, "expected to not be empty"])
+
+  asserts("when hash is not empty") do
+    assert_empty({:boo => "blux"})
+  end.equals([:pass, "is empty"])
+
+  asserts("when hash is empty") do
+    assert_empty({})[0..1]
+  end.equals([:fail, "expected to not be empty"])
+end # A negative empty assertion macro

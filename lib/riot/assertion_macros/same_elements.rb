9d9cdb3763d18ab1ad08444d570909659639951b
Arthur Chiu <Mr.Arthur.Chiu@gmail.com>
2010-10-06 08:30:41 -0700

    negative same elements macro

--- lib/riot/assertion_macros/same_elements.rb
+++ lib/riot/assertion_macros/same_elements.rb
@@ -4,11 +4,17 @@ module Riot
   #   should("test") { ["foo", "bar"] }.same_elements(["bar", "foo"])
   class SameElementsMacro < AssertionMacro
     register :same_elements
-
+    require 'set'
+    
     def evaluate(actual, expected)
-      require 'set'
       same = (Set.new(expected) == Set.new(actual))
       same ? pass : fail(expected_message.elements(expected).to_match(actual))
     end
+
+    def devaluate(actual, expected)
+      same = (Set.new(expected) == Set.new(actual))
+      same ? fail(expected_message.elements(expected).not.to_match(actual)) : pass
+    end
+
   end
 end
--- test/core/assertion_macros/same_elements_test.rb
+++ test/core/assertion_macros/same_elements_test.rb
@@ -15,3 +15,20 @@ context "A same_elements assertion macro" do
     topic.same_elements(["foo", "bar", 96])
   end
 end # A same_elements assertion macro
+
+context "A negative same_elements assertion macro" do
+  setup { Riot::Assertion.new("test", true) { ["foo","bar", 69] } }
+  
+  assertion_test_fails("when elements match", %Q{expected elements [69, "foo", "bar"], not to match ["foo", "bar", 69]}) do
+    topic.same_elements([69, "foo", "bar"])
+  end
+
+  assertion_test_fails("when any elements match", %Q{expected elements ["foo", "bar", 69], not to match ["foo", "bar", 69]}) do
+    topic.same_elements(["foo", "bar", 69])
+  end
+
+  assertion_test_passes("when elements do not match") do
+    topic.same_elements(["foo", "bar", 96])
+  end
+  
+end # A negative same_elements macro

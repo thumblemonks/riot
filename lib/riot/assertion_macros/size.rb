d7085ffc988d35dcfa1f84eb29d57f99f1300b74
Arthur Chiu <Mr.Arthur.Chiu@gmail.com>
2010-10-10 07:19:19 +0800

    negative size assertion macro

--- lib/riot/assertion_macros/size.rb
+++ lib/riot/assertion_macros/size.rb
@@ -11,5 +11,10 @@ module Riot
       failure_message = expected_message.size_of(actual).to_be(expected).not(actual.size)
       expected === actual.size ? pass(new_message.is_of_size(expected)) : fail(failure_message)
     end
+
+    def devaluate(actual, expected)
+      failure_message = expected_message.size_of(actual).to_not_be(expected).not(actual.size)
+      expected === actual.size ? fail(failure_message) : pass(new_message.is_not_size(expected))
+    end
   end
 end
--- test/core/assertion_macros/size_test.rb
+++ test/core/assertion_macros/size_test.rb
@@ -1,11 +1,7 @@
 require 'teststrap'
 
 context "A size assertion macro" do
-  setup do
-    def assert_size(sizable, expected_size)
-      Riot::Assertion.new("test") { sizable }.size(expected_size)
-    end
-  end
+  helper(:assert_size) { |sizable, expected_size| Riot::Assertion.new("test") { sizable }.size(expected_size) }
 
   assertion_test_passes("when string's size is as expected", "is of size 10") do
     assert_size("washington", 10)
@@ -43,5 +39,49 @@ context "A size assertion macro" do
   end
   assertion_test_fails("when a hash size is out of range", "expected size of {} to be 2...4, not 0") do
     assert_size({}, 2...4)
+  end
+end
+
+context "A negative size assertion macro" do
+  helper(:deny_size) { |sizable, expected_size| Riot::Assertion.new("test", true) { sizable }.size(expected_size) }
+
+  assertion_test_fails("when string's size is as expected", 'expected size of "washington" to not be 10, not 10') do
+    deny_size("washington", 10)
+  end
+  assertion_test_fails("when string's size is in given range", 'expected size of "washington" to not be 9..12, not 10') do
+    deny_size("washington", 9..12)
+  end
+
+  assertion_test_passes("when string's size is not as expected", 'is not size 11') do
+    deny_size("washington", 11)
+  end
+  assertion_test_passes("when string's size is out of range", 'is not size 11..13') do
+    deny_size("washington", 11..13)
+  end
+
+  assertion_test_fails("when an array's size is as expected", 'expected size of [1, 2, 3] to not be 3, not 3') do
+    deny_size([1, 2, 3], 3)
+  end
+  assertion_test_fails("when an array's size is in given range", 'expected size of [1, 2, 3] to not be 3..4, not 3') do
+    deny_size([1, 2, 3], 3..4)
+  end
+  assertion_test_passes("when an array's size is not as expected", "is not size 2") do
+    deny_size([1, 2, 3], 2)
+  end
+  assertion_test_passes("when an array's size is out of range", "is not size 4..6") do
+    deny_size([1, 2, 3], 4..6)
+  end
+
+  assertion_test_fails("when a hash size is as expected", 'expected size of {:a=>"b"} to not be 1, not 1') do
+    deny_size({:a => 'b'}, 1)
+  end
+  assertion_test_fails("when a hash size is in range", 'expected size of {:a=>"b"} to not be 1...3, not 1') do
+    deny_size({:a => 'b'}, 1...3)
+  end
+  assertion_test_passes("when a hash size is not as expected", "is not size 2") do
+    deny_size({}, 2)
+  end
+  assertion_test_passes("when a hash size is out of range", "is not size 2...4") do
+    deny_size({}, 2...4)
   end
 end

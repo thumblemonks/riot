31ea537fdf3b28dcaeb3eb077f674e86fb3668da
Justin Knowlden <gus@gusg.us>
2011-01-31 08:15:30 -0600

    [yard] Added doc for Message. Handling phrase arguments more appropriately. Privatized push

--- lib/riot/message.rb
+++ lib/riot/message.rb
@@ -3,21 +3,102 @@ class BlankSlate
 end
 
 module Riot
+  # A Message is similar in nature (but not implementation) to a string buffer; you put some strings in and
+  # calling {#to_s} will generate a single new string. What's special abnout Message is how you get strings
+  # into it. By convention, any method called on a Message that isn't defined will have its name turned into
+  # a string and any underscores replaced with spaces. This happens for each method call and those small
+  # messages are chained together at the end. For instance:
+  #
+  #   message = Riot::Message.new
+  #   message.hello_world.to_s
+  #    => "hello world"
+  #   
+  #   message.whats_the_news.to_s
+  #    => "hello world whats the news"
+  #
+  # For every method called it is also acceptable to pass any number of arguments. These arguments will be
+  # added to the final message after having {Kernel#inspect} called on them. Another for instance:
+  #
+  #   message = Riot::Message.new
+  #   message.expected([1, 2, 3], "foo").not([3, 2, 1], "bar")
+  #   message.to_s
+  #    => 'expected [1, 2, 3], "foo", not [3, 2, 1], "bar"'
+  #
+  # This is useful for - and was originally intended for - generating pass/fail messages from
+  # {Riot::AssertionMacro assertion macros}.
   class Message < BlankSlate
+
+    # Creates a new Message instance.
+    #
+    # @param [Array<Object>] *phrases an array of objects to be inspected
     def initialize(*phrases)
-      @chunks = []; _inspect(phrases)
+      @chunks = []
+      _inspect(phrases)
     end
 
+    # Generates the string value of the built-up message.
+    #
+    # @return [String]
     def to_s; @chunks.join.strip; end
+    alias_method :inspect, :to_s
 
-    def method_missing(meth, *phrases, &blk) push(meth.to_s.gsub('_', ' ')); _inspect(phrases); end
+    # Converts any method call into a more readable string by replacing underscores with spaces. Any
+    # arguments to the method are inspected and appended to the final message. Blocks are currently ignored.
+    #
+    # @param [String, Symbol] meth the method name to be converted into a more readable form
+    # @param [Array<Object>] *phrases an array of objects to be inspected
+    # @return [Riot::Message] this instance for use in chaining calls
+    def method_missing(meth, *phrases, &block)
+      push(meth.to_s.gsub('_', ' '))
+      _inspect(phrases)
+    end
 
-    def comma(str, *phrases) _concat([", ", str]); _inspect(phrases); end
+    # Adds a comma then the provided phrase to this message.
+    #
+    #   Riot::Message.new.hello.comma("world").to_s
+    #    => "hello, world"
+    #
+    # @param [String] str any string phrase to be added after the comma
+    # @param [Array<Object>] *phrases an array of objects to be inspected
+    # @return [Riot::Message] this instance for use in chaining calls
+    def comma(str, *phrases)
+      _concat([", ", str])
+      _inspect(phrases)
+    end
+
+    # Adds the string ", but".
+    #
+    #   Riot::Message.new.any_number.but(52).to_s
+    #    => "any number, but 52"
+    #
+    # @param [Array<Object>] *phrases an array of objects to be inspected
+    # @return [Riot::Message] this instance for use in chaining calls
     def but(*phrases); comma("but", *phrases); end
+
+    # Adds the string ", not".
+    #
+    #   Riot::Message.new.expected_freebies.not("$1.50").to_s
+    #    => 'expected freebies, not "$1.50"'
+    #
+    # @param [Array<Object>] *phrases an array of objects to be inspected
+    # @return [Riot::Message] this instance for use in chaining calls
     def not(*phrases); comma("not", *phrases); end
-    def push(str) _concat([" ", str]); end
+
   private
-    def _concat(chunks) @chunks.concat(chunks); self; end
-    def _inspect(phrases) phrases.each { |phrase| push(phrase.inspect) }; self; end
+    def push(str)
+      _concat([" ", str])
+    end
+
+    def _concat(chunks)
+      @chunks.concat(chunks)
+      self
+    end
+
+    def _inspect(phrases)
+      unless phrases.empty?
+        push(phrases.map { |phrase| phrase.inspect }.join(", "))
+      end
+      self
+    end
   end # Message
 end # Riot
--- test/core/runnable/message_test.rb
+++ test/core/runnable/message_test.rb
@@ -5,20 +5,20 @@ context "A message object" do
 
   asserts("opening phrase has inspected values") do
     Riot::Message.new("bar", nil, {:a => 2}).to_s
-  end.equals(%q["bar" nil {:a=>2}])
-
-  asserts("+ pushes phrase onto string") do
-    Riot::Message.new.mommy.push("dearest").to_s
-  end.equals(%q[mommy dearest])
+  end.equals(%q["bar", nil, {:a=>2}])
 
   context "receiving calls for unbound methods" do
     asserts("message") do
       Riot::Message.new.bar.to_s
     end.equals(%q[bar])
 
-    asserts("message with args") do
+    asserts("message with an arguments") do
       Riot::Message.new("Foo").bar("baz").to_s
     end.equals(%q["Foo" bar "baz"])
+
+    asserts("message with multiple arguments") do
+      Riot::Message.new("Foo").bar("baz", "boo", [1,2,3]).to_s
+    end.equals(%q{"Foo" bar "baz", "boo", [1, 2, 3]})
 
     asserts("in a long chain with underscored words") do
       Riot::Message.new.bar.baz.your_mom.to_s
@@ -32,4 +32,8 @@ context "A message object" do
 
   asserts("#not") { Riot::Message.new.not.to_s }.equals(", not")
   asserts("#not with message") { Riot::Message.new("Foo").not("a").to_s }.equals(%q["Foo", not "a"])
+
+  asserts("calling with inspect") do
+    Riot::Message.new.happy_nappy.inspect.to_s
+  end.equals("happy nappy")
 end # A message object

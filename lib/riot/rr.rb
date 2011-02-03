bbe1e0ce0d3c55656da20df320dc0b2974d809ac
Justin Knowlden <gus@gusg.us>
2011-02-03 08:57:59 -0600

    [yard] Added rdoc for Riot::RR support

--- lib/riot/rr.rb
+++ lib/riot/rr.rb
@@ -1,13 +1,29 @@
 require 'rr'
 
 module Riot
+  # Enables inherent RR support in Riot. When required in, all contexts and assertions are adapted to have
+  # RR support.
   module RR
 
+    # Basically, provides a {Riot::Situation} that has been adapted to RR. This means that any of the
+    # RR methods that would typically be used: +mock+, +stub+, +verify+, etc. will be available to any
+    # setup, yeardown, helper, hookup, or assertion.
     class Situation < Riot::Situation
       include ::RR::Adapters::RRMethods
     end # Situation
 
+    # Binds the {Riot::Assertion} to RR so that successes and failures found by RR are inherently handled
+    # during an assertion evaluation. In effect, if RR suggests failure during validation, the assertion
+    # will fail and report these failures.
     class Assertion < Riot::Assertion
+      # Adds RR support to {Riot::Assertion#run}. The basic flow is to:
+      # * run the test as normal
+      # * ask RR to verify mock results
+      # * report any errors or return the result of the assertion as normal
+      # * reset RR so that the next assertion in the context can be verified cleanly.
+      #
+      # @param (see Riot::Assertion#run)
+      # @return (see Riot::Assertion#run)
       def run(situation)
         result = super
         situation.verify
@@ -19,12 +35,18 @@ module Riot
       end
     end # Assertion
 
+    # Redefines the classes {Riot::Context} will use when creating new assertions and situations to be the
+    # ones provided RR support. See {Riot::RR::Assertion} and {Riot::RR::Situation}.
     module ContextHelpers
     private
       def assertion_class; Riot::RR::Assertion; end
       def situation_class; Riot::RR::Situation; end
     end # ContextHelpers
 
+    # A convenience method for telling {Riot::RR::ContextHelpers} to mix itself into {Riot::Context}. Thus,
+    # enabling RR support in Riot.
+    #
+    # @param [Class] context_class the class representing the Context to bind to
     def self.enable(context_class)
       context_class.instance_eval { include Riot::RR::ContextHelpers }
     end
